7481237ae6129b08c49f076855feb617181dc62a
Lukas Jaros <ooooidrr@gmail.com>
2026-06-17 12:21:34 +0200

    Matching-Modul hinzugefügt

--- HRomance/Services/MatchingService.cs
+++ HRomance/Services/MatchingService.cs
@@ -1,6 +1,33 @@
-﻿namespace HRomance.Services
+﻿using HRomance.Data;
+using HRomance.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRomance.Services
 {
     public class MatchingService
     {
+        private readonly ApplicationDbContext _context;
+
+        public MatchingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Mitarbeiter>> GetPassendeMitarbeiterAsync(int auftragId)
+        {
+            var auftrag = await _context.Auftraege
+                .FirstOrDefaultAsync(a => a.Id == auftragId);
+
+            if (auftrag == null)
+            {
+                return new List<Mitarbeiter>();
+            }
+
+            return await _context.Mitarbeiter
+                .Where(m =>
+                    m.Verfuegbar &&
+                    m.Qualifikation == auftrag.BenoetigteQualifikation)
+                .ToListAsync();
+        }
     }
 }
