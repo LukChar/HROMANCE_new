b651ea9ba59dbb01bc4bfb870b9db49f3caa9bd1
Lukas Jaros <ooooidrr@gmail.com>
2026-06-17 11:59:26 +0200

    Kundenverwaltung hinzugefügt

--- HRomance/Services/KundeService.cs
+++ HRomance/Services/KundeService.cs
@@ -1,6 +1,49 @@
-﻿namespace HRomance.Services
+﻿using HRomance.Data;
+using HRomance.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HRomance.Services
 {
     public class KundeService
     {
+        private readonly ApplicationDbContext _context;
+
+        public KundeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Kunde>> GetAllAsync()
+        {
+            return await _context.Kunden.ToListAsync();
+        }
+
+        public async Task<Kunde?> GetByIdAsync(int id)
+        {
+            return await _context.Kunden.FindAsync(id);
+        }
+
+        public async Task AddAsync(Kunde kunde)
+        {
+            _context.Kunden.Add(kunde);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Kunde kunde)
+        {
+            _context.Kunden.Update(kunde);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var kunde = await _context.Kunden.FindAsync(id);
+
+            if (kunde != null)
+            {
+                _context.Kunden.Remove(kunde);
+                await _context.SaveChangesAsync();
+            }
+        }
     }
 }
